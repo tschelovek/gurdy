af1477ca238682691d8ea975ecb79de56209ebfd
tschelovek <az18rg@gmail.com>
2023-04-18 13:54:16 +0300

    0.3.0

--- js/main.js
+++ js/main.js
@@ -1,127 +1,129 @@
 document.addEventListener('DOMContentLoaded', () => {
-    /**
-     *
-     * МОДАЛЬНОЕ ОКНО
-     * buttons {NodeListOf<Element>} - собираем все кнопки с классом .callback_init
-     * чтоб повесить хэндлер открытия модального окна
-     *
-     */
-    const POST_FORM_URL = '/telegram-bot/';
-    const SUCCESS_MESSAGE = 'Ваша заявка успешно отправлена';
-    const ERROR_MESSAGE = 'При отправке произошли проблемы! Повторите попытку позже или свяжитесь с нами иными способами!';
-    const modal = document.getElementById('modal');
+  /**
+   *
+   * МОДАЛЬНОЕ ОКНО
+   * buttons {NodeListOf<Element>} - собираем все кнопки с классом .callback_init
+   * чтоб повесить хэндлер открытия модального окна
+   *
+   */
+  const POST_FORM_URL = '/telegram-bot/';
+  const SUCCESS_MESSAGE = 'Ваша заявка успешно отправлена';
+  const ERROR_MESSAGE = 'При отправке произошли проблемы! Повторите попытку позже или свяжитесь с нами иными способами!';
+  const modal = document.getElementById('modal');
 
-    modal.querySelector('.modal__dialog').addEventListener('click', e => {
-        if (e.target === e.currentTarget) handlerCloseModal()
-    });
-    modal.querySelector('.btn_close').addEventListener('click', () => handlerCloseModal());
-    document.querySelectorAll('.callback_init')
-        .forEach(button => button.addEventListener('click', e => handlerOpenCallback(e.currentTarget)));
+  modal.querySelector('.modal__dialog').addEventListener('click', e => {
+    if (e.target === e.currentTarget) handlerCloseModal()
+  });
+  modal.querySelector('.btn_close').addEventListener('click', () => handlerCloseModal());
+  document.querySelectorAll('.callback_init')
+    .forEach(button => button.addEventListener('click', e => handlerOpenCallback(e.currentTarget)));
+  document.form_callback.addEventListener('submit', async e => {
+    e.preventDefault();
 
-    function handlerOpenCallback(eTarget) {
-        modal.style.display = 'block';
-        setTimeout(() => modal.classList.add('open'), 50);
+    const form = document.getElementById('form_callback');
+    const messageContainer = modal.querySelector('.modal__status');
+    const data = new FormData(form)
 
-        modal.querySelector('.btn_submit').textContent = eTarget.textContent;
-        modal.querySelector('.modal__title').textContent = eTarget.dataset.title;
-
-        document.addEventListener(
-            'keydown',
-            e => { if (e.key === "Escape") handlerCloseModal() },
-            { once: true }
-        )
-    }
-
-    function handlerCloseModal() {
-        modal.classList.remove('open');
-        modal.querySelector('.modal__status').textContent = '';
-        setTimeout(() => modal.style.display = 'none', 600);
-    }
-
-    modal.addEventListener('submit', async e => {
-        e.preventDefault();
-
-        const form = document.getElementById('form_callback');
-        const messageContainer = modal.querySelector('.modal__status');
-        const data = new FormData(form)
-
-        try {
-            await postData(POST_FORM_URL, data)
-                .then(() => {
-                    messageContainer.textContent = SUCCESS_MESSAGE;
-                })
-        } catch (err) {
-            messageContainer.textContent = ERROR_MESSAGE;
-            console.error(err);
-        }
-    })
-
-    async function postData(url = '', data = {}) {
-        await fetch(url, {
-            method: 'POST',
-            mode: 'cors',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'form/multipart'
-            },
-            body: data
+    try {
+      await postData(POST_FORM_URL, data)
+        .then(() => {
+          messageContainer.textContent = SUCCESS_MESSAGE;
         })
-            .catch(() => {
-                throw Error
-            })
+    } catch (err) {
+      messageContainer.textContent = ERROR_MESSAGE;
+      console.error(err);
     }
+  })
 
-    /**
-     *
-     * Бургер меню
-     */
-    document.getElementById('burgerButton')?.addEventListener('click', e => {
-        e.preventDefault();
-        e.currentTarget.classList.toggle('active');
-        document.querySelector('.nav_header')?.classList.toggle('active');
+  function handlerOpenCallback(eTarget) {
+    modal.style.display = 'block';
+    setTimeout(() => modal.classList.add('open'), 50);
+
+    modal.querySelector('.btn_submit').textContent = eTarget.textContent;
+    modal.querySelector('.modal__title').textContent = eTarget.dataset.title;
+    document.form_callback.elements.modalName.value = eTarget.textContent;
+
+    document.addEventListener(
+      'keydown',
+      e => {
+        if (e.key === "Escape") handlerCloseModal()
+      },
+      {once: true}
+    )
+  }
+
+  function handlerCloseModal() {
+    modal.classList.remove('open');
+    modal.querySelector('.modal__status').textContent = '';
+    setTimeout(() => modal.style.display = 'none', 600);
+  }
+
+  async function postData(url = '', data = {}) {
+    await fetch(url, {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'same-origin',
+      headers: {
+        'Content-Type': 'form/multipart'
+      },
+      body: data
     })
+      .catch(() => {
+        throw Error
+      })
+  }
 
-    /**
-     *
-     * @type {Swiper} Инициализация слайдера
-     */
-    const swiper = new Swiper('#slider_advantages', {
-        pagination: {
-            el: '.swiper-pagination',
+  /**
+   *
+   * Бургер меню
+   */
+  document.getElementById('burgerButton')?.addEventListener('click', e => {
+    e.preventDefault();
+    e.currentTarget.classList.toggle('active');
+    document.querySelector('.nav_header')?.classList.toggle('active');
+  })
+
+  /**
+   *
+   * @type {Swiper} Инициализация слайдера
+   */
+  const swiper = new Swiper('#slider_advantages', {
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+    loop: true,
+    autoplay: {
+      delay: 3000,
+    },
+    speed: 1000,
+    breakpoints: {
+      640: {
+        autoplay: false,
+        grid: {
+          rows: 4
         },
-        // navigation: {
-        //     nextEl: '.swiper-button-next',
-        //     prevEl: '.swiper-button-prev',
-        // },
-        loop: true,
-        autoplay: {
-            delay: 3000,
-        },
-        speed: 1000,
-        breakpoints: {
-            640: {
-                autoplay: false,
-                grid: {
-                    rows: 4
-                },
-                allowTouchMove: false,
-            },
-        }
-    })
+        allowTouchMove: false,
+      },
+    }
+  })
 
-    /**
-     * Parallax
-     */
-    const parallaxInstance1 = new Parallax(document.getElementById('scene'));
+  /**
+   * Parallax
+   */
+  const parallaxInstance1 = new Parallax(document.getElementById('scene'));
 
-    /**
-     * Scroll progress-bar
-     */
-    const scrollProgress = document.getElementById('scroll-progress');
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  /**
+   * Scroll progress-bar
+   */
+  const scrollProgress = document.getElementById('scroll-progress');
+  const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
 
-    window.addEventListener('scroll', () => {
-        const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
-        scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
-    });
+  window.addEventListener('scroll', () => {
+    const scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+    scrollProgress.style.width = `${(scrollTop / height) * 100}%`;
+  });
 })
